f6ffd4d616cc15d7eaefadc45793af7015a01dfa
Luis Badiali <info@badiali.es>
2020-07-30 15:47:22 +0200

    Corrección favicon.ico

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = {
         }]
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
@@ -92,12 +92,25 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      favicon: './src/favicon.ico',
       minify: {
         collapseWhitespace: true
       },
@@ -106,6 +119,7 @@ module.exports = {
       chunks: ['app','index']
     }),
     new HtmlWebpackPlugin({
+      favicon: './src/favicon.ico',
       minify: {
         collapseWhitespace: true
       },
